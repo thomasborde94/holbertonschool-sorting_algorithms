40de15869335ac6bec86bb2186f48f1318fa0bcd
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-12-26 02:28:49 -0800

    adding space after '}' in line 19

--- sort.h
+++ sort.h
@@ -16,7 +16,7 @@ typedef struct listint_s
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
