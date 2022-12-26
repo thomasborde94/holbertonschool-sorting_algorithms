19e544787df9e506d2ecb4b594286a31964c0592
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-12-26 02:20:23 -0800

    a function that sorts an array of integers in ascending order using the Quick sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * quick_sort - a function that sorts an array of integers
+ * @array: array of integer
+ * @size: size of the structure
+ **/
+void quick_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int pivot, temp;
+
+	if (size < 2)
+		return;
+
+	pivot = array[size / 2];
+	for (i = 0, j = size - 1;; i++, j--)
+	{
+		while (array[i] < pivot)
+			i++;
+		while (array[j] > pivot)
+			j--;
+
+		if (i >= j)
+			break;
+
+		temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	quick_sort(array, i);
+	quick_sort(array + i, size - i);
+}
